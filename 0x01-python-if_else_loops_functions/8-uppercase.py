b0ecf5afae95e67f7650017cf81df387a20789ab
rawan-omer <nanaomer0119@gmail.com>
2023-11-29 15:43:31 -0800

    alx-higher_level_programming

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        if ord(str[i]) >= 97 and ord(str[i]) < 123:
-            ord(str[i]) -= 32
+        r = ord(str[i])
+        if r >= 97 and r < 123:
+            r -= 32
         else:
-            print("{}".format(chr(ord(str[i]))), end="")
+            print("{}".format(chr(r)), end="")
     print()
