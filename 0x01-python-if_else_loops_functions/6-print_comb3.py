8ddb4f6eddbfd385175a2eb75fd759f72d957883
rawan-omer <nanaomer0119@gmail.com>
2023-11-29 15:53:23 -0800

    alx-higher_level_programming

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,7 @@ for r in range(0, 10):
     for c in range(1, 10):
         if r >= c:
             continue
-        elif r != 8 and c != 9:
-            print("{}{}, ".format(r, c), end="")
-        else:
+        elif r == 8 and c == 9:
             print("{}{}".format(r, c))
+        else:
+            print("{}{}, ".format(r, c), end="")
