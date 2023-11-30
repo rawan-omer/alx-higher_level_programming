77e490d6721bd4cc56d30204321e1715f4feae16
rawan-omer <nanaomer0119@gmail.com>
2023-11-30 14:01:19 -0800

    alx-higher_level_programming

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, mal(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
