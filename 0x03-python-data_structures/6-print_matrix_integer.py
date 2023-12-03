e39f144936fc3d4c8844ff834a9fb91545d5f271
rawan-omer <nanaomer0119@gmail.com>
2023-12-03 12:10:16 -0800

    alx-higher_level_programming

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,8 @@ def print_matrix_integer(matrix=[[]]):
         n = 1
         for i in row:
             if n == len(row):
-                print("{:d}".format(i))
+                print("{:d}".format(i), end="")
             else:
                 print("{:d}".format(i), end=" ")
             n += 1
+        print()
