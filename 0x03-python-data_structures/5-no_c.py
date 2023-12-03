8329bb918a6c63b03b0a21aad8d9edd39db44038
rawan-omer <nanaomer0119@gmail.com>
2023-12-03 11:40:16 -0800

    alx-higher_level_programming

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    myList = list(my_string)
-    for char in myList:
-        if char == 'c' or char == 'C':
-            myList.remove(char)
-    return (myList)
+    myList = []
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            myList.append(char)
+    return ''.join(myList)
