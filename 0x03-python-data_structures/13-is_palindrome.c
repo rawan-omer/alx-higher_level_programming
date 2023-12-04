136215b6ffedd467f56c9371e544ff6b4c9fe882
rawan-omer <nanaomer0119@gmail.com>
2023-12-04 12:15:51 -0800

    alx-higher_level_programming

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,38 @@
+#include "list.h"
+/**
+ * is_palindrome - check if the number is palindrome
+ * @head: the head of list
+ * Return: integer
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int i, j, r;
+	int *array;
+	listint_t *c = *head;
+
+	if (head == 0 || *head == 0)
+		return (1);
+	for (i = 0; c != 0; i++)
+		c = c->next;
+	array = malloc(i * sizeof(int));
+	if (array == 0)
+		return (0);
+	c = *head;
+	for (j = 0; j < i; j++)
+	{
+		array[j] = c->n;
+		c = c->next;
+	}
+	for (j = 0, r = i - 1; j < r; j++, r--)
+	{
+		if (array[j] != array[r])
+		{
+			free(array);
+			return (0);
+		}
+	}
+
+	free(array);
+	return (1);
+}
