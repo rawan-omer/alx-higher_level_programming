f9c7612f360b630653c5f562a66d238de07ede47
rawan-omer <nanaomer0119@gmail.com>
2023-12-06 15:09:13 -0800

    alx-higher_level_programming

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    return (my_list)
+    return [replace if num == search else num for num in my_list]
