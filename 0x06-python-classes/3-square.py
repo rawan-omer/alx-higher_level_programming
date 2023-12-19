ca50b9e724a70ed7fc8ac212b85a808ea1d60f7b
rawan-omer <rawanomer0119@gmail.com>
2023-12-19 21:53:11 +0000

    alx-higher_level_programming

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Class that defines square"""
+    def __init__(self, size=0):
+        """Constructor
+
+        Args:
+            size: squer's length
+        """
+        self.__size = size
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """The area of square.
+
+        Returns:
+		The area squared.
+        """
+        return self.__size * size
