24797671ef9e3616f177edaf914f3d3595f51418
rawan-omer <rawanomer0119@gmail.com>
2023-12-20 22:31:11 +0000

    alx-higher_level_programming

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+	divid_list = []
+	divid = 0
+	for n in range(0, lisr_length)
+		try:
+			divid = (my_list_1[i] / my_list_2[i])
+		except TypeError:
+			divid = 0
+			print("wrong type")
+		except IndexError:
+			divid = 0
+			print("out of range")
+		except ZeroDivisionError:
+			divid = 0
+			print("division by 0")
+		finally:
+			divid_list.append(divid)
+	return (divid_list)
