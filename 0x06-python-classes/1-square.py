c346878763e1cee6b240ba15461dc4b2e8482108
rawan-omer <rawanomer0119@gmail.com>
2023-12-20 22:56:09 +0000

    alx-higher_level_programming

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
 class Square:
-	"""Square that defines a square"""
+	"""Square that defines a square
+
+	Attributes:
+		__size (int): The length of the square's sides.
+	"""
 
 	def __init__(self, size):
 		"""Initialises the data
+
 		Args:
 			size: square length
 		"""
