b36a4984fc33eaa83139eae66341bf456cbd89dd
rawan-omer <rawanomer0119@gmail.com>
2024-01-03 07:26:07 +0000

    alx-higher_level_programming

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+''' Function that add 2 numbers '''
+def add_integer(a, b=98):
+	if not isinstance(a, (int, float)):
+		raise TypeError("a must be an integer or b must be an integer")
+
+	if not isinstance(b, (int, float)):
+		raise TypeError("b must be an integer or b must be an integer")
+
+	b = int(b)
+	a = int(a)
+
+	return (a + b)
