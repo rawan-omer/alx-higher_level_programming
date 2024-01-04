6f74a485de52e4e34b3599d7830907b8a135d880
rawan-omer <rawanomer0119@gmail.com>
2024-01-04 21:36:46 +0000

    alx-heigher_level_programming

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -67,5 +67,5 @@ class Rectangle:
     def perimeter(self):
         """returns the perimeter of rectangle"""
         if self.__height == 0 or self.__width == 0:
-            return 0
-    return 2 * (self.__width + self.__height)
+            return
+        return 2 * (self.__width + self.__height)
