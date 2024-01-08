50817505627e4fd6e84dfe5d358d9f09b015c386
rawan-omer <rawanomer0119@gmail.com>
2024-01-08 22:12:00 +0000

    alx-heigher_level_programming

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,8 @@
 
 class MyList(list):
     """class MyList that inherits from list"""
+
+    print("right")
     def print_sorted(self):
         """prints the list, but sorted"""
         print(sorted(self))
