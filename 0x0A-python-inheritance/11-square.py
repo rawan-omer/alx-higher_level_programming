5d9daf1dd38868caad03d1cbdbe2e2c8ada39365
rawan-omer <rawanomer0119@gmail.com>
2024-01-08 21:10:48 +0000

    alx-heigher_level_programming

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -8,10 +8,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """Instantiation with size"""
-
+        super().__init__(size, size)
         self.integer_validator("size", size)
         self.__size = size
-        super().__init__(size, size)
 
     def area(self):
         """method must be implemented"""
