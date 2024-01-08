b1b5402ea63bccb29e7a2508a5d581651233ce38
rawan-omer <rawanomer0119@gmail.com>
2024-01-08 14:52:42 +0000

    alx-heigher_level_programming

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""lookup method"""
+
 
 def lookup(obj):
     """ function that returns the list of available attributes"""
-    return dir(obj)
+    return [i for i in dir(obj)]
