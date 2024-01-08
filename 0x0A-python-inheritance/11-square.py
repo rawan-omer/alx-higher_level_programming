91c83251c369fafa691335564de476f084e897af
rawan-omer <rawanomer0119@gmail.com>
2024-01-08 20:53:26 +0000

    alx-heigher_level_programming

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -5,8 +5,10 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 class Square(Rectangle):
     """Square class that inherits from Rectangle"""
+
     def __init__(self, size):
         """Instantiation with size"""
+
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
@@ -17,4 +19,4 @@ class Square(Rectangle):
 
     def __str__(self):
         """representation of the string"""
-        return "[Square] {}/{}".format(self.__size, self.__size) + super().__str__()
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
