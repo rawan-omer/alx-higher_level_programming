aa4bc69a8607ce539dbf54a58e7fde7f16c4f76b
rawan-omer <rawanomer0119@gmail.com>
2024-01-08 19:34:19 +0000

    alx-heigher_level_programming

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry Class"""
+
+
+class BaseGeometry:
+    """BaseGeometry (based on 5-base_geometry.py)."""
+    def area(self):
+        """raises an Exception with message"""
+        raise Exception("area() is not implemented")
