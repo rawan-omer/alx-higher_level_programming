ae17781f80caa56486dba5328843ae18963bc9fa
rawan-omer <rawanomer0119@gmail.com>
2024-01-09 21:25:46 +0000

    alx-heigher_level_programming

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,13 @@
 
 
 import sys
-from os.path import exists
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 alist = list(sys.argv[1:])
 try:
-    if exists('add_item.json'):
-        old_data = load_from_json_file('add_item.json')
-    else:
-        old_data = []
-except FileNotFoundError:
+    old_data = load_from_json_file('add_item.json')
+except Exception:
     old_data = []
 
 old_data.extend(alist)
