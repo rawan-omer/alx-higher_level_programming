709ae6757b28fe22fe8d7803d6e404a51af39dfb
rawan-omer <rawanomer0119@gmail.com>
2024-01-09 22:24:34 +0000

    alx-heigher_level_programming

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Student class Definition"""
 
+
 class Student:
+
     def __init__(self, first_name, last_name, age):
         """Initialize Student class attributes"""
         self.first_name = first_name
