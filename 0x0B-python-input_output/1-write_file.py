0fa6fbf2f8e7bd0625c2673b4086313c7299d118
rawan-omer <rawanomer0119@gmail.com>
2024-01-09 20:01:02 +0000

    alx-heigher_level_programming

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """writes string to a text file and returns the number of characters"""
     with open(filename, "w", encoding='utf-8') as f:
-        return len(list(f))
+        f.write(text)
+    return len(text)
