55539112d6ee4aa8b84db493d092533c0e0d1f3e
rawan-omer <rawanomer0119@gmail.com>
2024-01-09 19:07:02 +0000

    alx-heigher_level_programming

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read_file definition"""
+
+
+def read_file(filename=""):
+    """reads a text file and prints it to stdout"""
+    with open(filename, encoding='UTF8') as f:
+        print(f.read, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write_file definition"""
+
+
+def write_file(filename="", text=""):
+    """writes string to a text file and returns the number of characters"""
+    with open(filename, encodeing='UTF8') as f:
+        return len(list(f))
