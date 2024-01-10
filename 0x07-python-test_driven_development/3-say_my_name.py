98a928bb3e088e6052f74dff0784274d466ba596
rawan-omer <rawanomer0119@gmail.com>
2024-01-10 23:11:17 +0000

    alx-heigher_level_programming

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -2,6 +2,8 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """prints My name is <first name> <last name>"""
+
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
