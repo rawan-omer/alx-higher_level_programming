1d14ede8d57cb82ddd911093ea2bdf67c61abf15
rawan-omer <rawanomer0119@gmail.com>
2024-01-10 18:40:45 +0000

    alx-heigher_level_programming

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def matrix_divided(matrix, div):
+    """divides all elements of a matrix."""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for lists in matrix:
+        if not isinstance(lists, list):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(len(lists) == len(matrix[0]) for lists in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(item / div, 2) for item in lists] for lists in matrix]
