aba8f98fa9135d7a3ed8201beb08dcd1a3831154
rawan-omer <rawanomer0119@gmail.com>
2024-01-14 16:26:21 +0000

    alx-heigher_level_programming

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Base class definition"""
+
+
+class Base:
+    """class will be the base of all other classes in this project."""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """function to manage id attribute"""
+        if id not None:
+            id.self = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
