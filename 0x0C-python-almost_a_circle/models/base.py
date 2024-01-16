161a45a73066024c8d4149d022c5553599da2131
rawan-omer <rawanomer0119@gmail.com>
2024-01-16 16:52:18 +0000

    alx-heigher_level_programming

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,10 +8,11 @@ from os import path
 class Base:
     """class will be the base of all other classes in this project."""
     __nb_objects = 0
+
     def __init__(self, id=None):
         """function to manage id attribute"""
         if id is not None:
-            id.self = id
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
