26f971cb1b003e61f3cfcf57870ccae6bc0a2cc8
rawan-omer <rawanomer0119@gmail.com>
2024-02-14 22:05:15 +0000

    alx-programming

--- 0-privileges.sql
+++ 0-privileges.sql
@@ -1,14 +1,4 @@
--- Create user_0d_1
-CREATE USER 'user_0d_1'@'localhost';
-
--- Grant user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user_0d_2
-CREATE USER 'user_0d_2'@'localhost';
-
--- Grant user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges
-FLUSH PRIVILEGES;
+-- script that lists all privileges of the MySQL user
+SHOW GRANTS FOR user_0d_1@localhost;
+-- the seconed user
+SHOW GRANTS FOR user_0d_2@localhost;
