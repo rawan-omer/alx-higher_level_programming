4a2ea4735c7e22103b947180ea589a8723f89065
rawan-omer <rawanomer0119@gmail.com>
2024-02-15 20:48:12 +0000

    alx-programming

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- Import the database dump of hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.tv_show_id
