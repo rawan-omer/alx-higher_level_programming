3cb2c44bdb1f49002a59d683ae51e9ed473876a8
rawan-omer <rawanomer0119@gmail.com>
2024-03-12 16:11:39 -0700

    alx-programming

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function(list, searchElement) {
+    let count = 0;
+    for (let element of list) {
+        if (element === searchElement) {
+            count++;
+        }
+    }
+    return count;
+};
