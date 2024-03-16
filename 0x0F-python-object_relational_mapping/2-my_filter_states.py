20c6c7a12e19144583b9e2fd66133a6ecc00b720
rawan-omer <rawanomer0119@gmail.com>
2024-03-16 06:47:33 -0700

    alx-programming

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,21 +1,24 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""Displays values in the states table where name matches the argument"""
+
 import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
+    state_name = sys.argv[4]
 
     try:
         db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
         cursor = db.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        cursor.execute(query)
 
         rows = cursor.fetchall()
         for r in rows:
@@ -27,4 +30,3 @@ if __name__ == '__main__':
     except MySQLdb.Error as e:
         print("Error connecting to MySQL:", e)
         sys.exit(1)
-
