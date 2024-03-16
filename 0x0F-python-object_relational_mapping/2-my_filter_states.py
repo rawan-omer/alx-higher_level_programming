35f226e621709e90acdf823273daa64010f4af21
rawan-omer <rawanomer0119@gmail.com>
2024-03-16 15:48:13 -0700

    alx-programming

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,8 @@ import sys
 
 if __name__ == '__main__':
     if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        print("Usage: {} username password database state_name"
+              .format(sys.argv[0]))
         sys.exit(1)
 
     username = sys.argv[1]
@@ -15,9 +16,11 @@ if __name__ == '__main__':
     state_name = sys.argv[4]
 
     try:
-        db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(host='localhost', port=3306,
+                             user=username, passwd=password, db=database)
         cursor = db.cursor()
-        query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(state_name)
+        query = ("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                 .format(state_name))
         cursor.execute(query)
 
         rows = cursor.fetchall()
