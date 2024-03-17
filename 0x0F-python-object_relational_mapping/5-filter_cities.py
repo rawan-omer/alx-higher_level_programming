51ef55bc5f7bfc8c6ba9dd85efac39d720d7ef7c
rawan-omer <rawanomer0119@gmail.com>
2024-03-17 06:48:42 -0700

    alx-programming

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,45 +1,42 @@
 #!/usr/bin/python3
 """Lists all cities of state from database hbtn_0e_4_usa"""
-
 import MySQLdb
-import sys
+from sys import argv, exit
 
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database state_name"
-              .format(sys.argv[0]))
-        sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    if len(argv) != 5:
+        print("Usage: {} username password database state_name"
+              .format(argv[0]))
+        exit(1)
 
     try:
         db = MySQLdb.connect(
             host='localhost',
             port=3306,
-            user=username,
-            passwd=password,
-            db=database
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3]
         )
         cursor = db.cursor()
         query = """
-                SELECT GROUP_CONCAT(name SEPARATOR ', ')
+                SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
                 FROM cities
                 JOIN states ON cities.state_id = states.id
                 WHERE states.name = %s
                 ORDER BY cities.id ASC
                 """
-        cursor.execute(query, (state_name,))
+        cursor.execute(query, (argv[4],))
 
         row = cursor.fetchone()
-        if row:
+        if row[0]:
             print(row[0])
+        else:
+            print("No cities found for the given state.")
 
         cursor.close()
         db.close()
 
     except MySQLdb.Error as e:
         print("Error connecting to MySQL:", e)
-        sys.exit(1)
+        exit(1)
