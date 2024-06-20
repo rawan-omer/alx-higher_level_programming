1399fe1cfd84df1627d7290a9277a2321730b9ee
rawan-omer <rawanomer0119@gmail.com>
2024-06-20 15:24:46 -0700

    alx-programming

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
 // prints My number: <first argument converted in integer>
 
-if (isNaN(process.argv[2])) {
-  console.log("Not a numbe");
+const arg = process.argv[2];
+
+const parsedInt = parseInt(arg);
+
+if (!isNaN(parsedInt)) {
+  console.log('My number: ${parsedInt}');
 } else {
-  console.log("My number: " + parseInt(process.argv[2]));
+  console.log("Not a number");
 }
